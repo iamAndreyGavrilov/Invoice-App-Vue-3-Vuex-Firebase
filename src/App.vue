9bdc3a762e8b4cd3fb88638340d6a4a382065776
Andrey Gavrilov <boombardir20@gmail.com>
2022-01-27 13:33:01 +0500

    add nav + mobile message

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,47 @@
 <template>
-  <router-view />
+  <div>
+    <div v-if="!mobile" class="app flex flex-column">
+      <Navigation />
+      <div class="app-content flex flex-column">
+        <router-view />
+      </div>
+    </div>
+    <div v-else class="mobile-message flex flex-column">
+      <h2>
+        Извините, это приложения не поддерживается на мобильных устройствах
+      </h2>
+      <p>Используйте компьютер или планшет</p>
+    </div>
+  </div>
 </template>
+
+<script>
+import Navigation from "@/components/Navigation.vue";
+export default {
+  data() {
+    return {
+      mobile: null,
+    };
+  },
+  components: {
+    Navigation,
+  },
+  methods: {
+    checkScreen() {
+      const windowWidth = window.innerWidth;
+      if (windowWidth <= 750) {
+        this.mobile = true;
+        return;
+      }
+      this.mobile = false;
+    },
+  },
+  created() {
+    this.checkScreen();
+    window.addEventListener("resize", this.checkScreen);
+  },
+};
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
@@ -10,7 +51,30 @@
   padding: 0;
   box-sizing: border-box;
   font-family: "Poppins", sans-serif;
+}
+.app {
   background-color: #141625;
+  min-height: 100vh;
+  @media (min-width: 900px) {
+    flex-direction: row !important;
+  }
+  .app-content {
+    padding: 0 20px;
+    flex: 1;
+    position: relative;
+  }
+}
+
+.mobile-message {
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #141625;
+  color: #fff;
+}
+p {
+  margin-top: 16px;
 }
 
 button,
