efe880e332ff0e88fcef9c644cd20f231d150a54
iamAndreyGavrilov <boombardir20@gmail.com>
2022-02-05 19:43:06 +0500

    null

---
+++ src/components/Invoice.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-link
+    class="invoice flex"
+    :to="{ name: 'Invoice', params: { invoiceId: invoice.invoiceId } }"
+  >
+    <div class="left flex">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: "invoice",
+  props: ["invoice"],
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,11 +25,20 @@
         </div>
       </div>
     </div>
+    <!-- Счета -->
+    <div>
+      <Invoice
+        v-for="(invoice, index) in invoiceData"
+        v-bind:invoice="invoice"
+        :key="index"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import { mapMutations } from "vuex";
+import Invoice from "../components/Invoice.vue";
+import { mapMutations, mapState } from "vuex";
 
 export default {
   name: "Home",
@@ -38,7 +47,7 @@ export default {
       filterMenu: null,
     };
   },
-  components: {},
+  components: { Invoice },
   methods: {
     ...mapMutations(["TOGGLE_INVOICE"]),
     newInvoice() {
@@ -47,6 +56,9 @@ export default {
     toggleFilterMenu() {
       this.filterMenu = !this.filterMenu;
     },
+  },
+  computed: {
+    ...mapState(["invoiceData"]),
   },
 };
 </script>
